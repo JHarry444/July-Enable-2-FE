a0b0a68b41cf0448f3509f5eaeec54bb8049b895
jordan <jharrison1@qa.com>
2022-08-03 09:31:41 +0100

    added functions example

---
+++ 03-Functions/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function add(a, b) {
+    return a + b;
+}
+
+add(12,34)
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+subtract(23, 12)
+
+multiply(2, 4)
+
+const divide = (a, b) => a / b;
+
+divide(25, 5)
+
+const print = var1 => console.log(var1);
+
+print("hello")
